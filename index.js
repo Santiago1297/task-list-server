b975b2ec7f2b36446c3b57e2dbfb41b79e5aa19a
Santiago Rivera <santiago1297@hotmail.es>
2023-09-19 11:06:54 -0500

    Se crea el servidor y se retorna task list en JSON

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Lista de tareas
+const tasks = [
+  {
+    id: '123456',
+    isCompleted: false,
+    description: 'Walk the dog',
+  },
+  // Puedes agregar más tareas aquí
+];
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
