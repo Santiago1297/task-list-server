de2be4fc112c9b7d7c63ca9092c138ac2f3329ad
Santiago Rivera <santiago1297@hotmail.es>
2023-11-25 16:00:19 -0500

    Se agrega la parte de autenticación

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
 // Importa el enrutador de listas
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
+const authRouter = require('./auth-router');
 
 // Middleware para gestionar solicitudes por métodos HTTP válidos
 app.use((req, res, next) => {
@@ -17,9 +22,13 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+// Middleware para proteger rutas con JWT
+app.use('/protected', authenticateToken);
+
 // Se asocia el enrutador de List View Router y List Edit Router
 app.use('/view', listViewRouter);
 app.use('/edit', listEditRouter);
+app.use('/login', authRouter);
 
 // Lista de tareas
 const tasks = [
@@ -43,3 +52,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
 });
+
+// Middleware para verificar el token JWT
+function authenticateToken(req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Acceso no autorizado' });
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Token no válido' });
+
+    req.user = user;
+    next();
+  });
+}
